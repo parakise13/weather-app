869658fba491ad5bec5c18cb44e88b8d7b643717
NahyeonKim <parakise13@gmail.com>
2022-06-17 22:14:36 +0900

    add favoriteCities first

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { AiOutlineClose, AiOutlinePlusSquare } from "react-icons/ai";
 import "./App.scss";
+import axios from "axios";
 
 interface WeatherDetailProps {
   main?: string;
@@ -14,28 +15,25 @@ interface WeatherMainProps{
   feels_like?: number;
 }
 
-interface WeatherProps {
+export interface WeatherProps {
   name?: string;
   main?: WeatherMainProps;
   weather?: WeatherDetailProps[];
+  id?: number;
 }
 
 function App() {
   const [data, setData] = useState<WeatherProps>({});
+  const [favoriteCities, setFavoriteCities] = useState<WeatherProps[]>([]);
   const [location, setLocation] = useState("");
-
   const APIKey = "64d28b810aabcf14458d21729721bc51";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIKey}`;
-
-  // const convertFtoC = (fahrenheit: number) => {
-  //   return (fahrenheit - 273.15).toFixed(1);
-  // };
+	
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIKey}`;
 
   const searchLocation = (evt:React.KeyboardEvent<HTMLInputElement>) => {
     if (evt.key === 'Enter') {
       axios.get(url).then((res) => {
         setData(res.data);
-        console.log(res.data)
         setLocation('');
       })
     }
@@ -43,6 +41,14 @@ function App() {
 
   const handleChangeValue = (evt: React.ChangeEvent<HTMLInputElement>) => {
     setLocation(evt.target.value);
+  }
+
+  const handleAddFavoriteCity = (data: WeatherProps) => {
+    // let 
+    const existingCityIndex = favoriteCities.findIndex((city) => city.name === data.name);
+    if (existingCityIndex === -1) {
+      setFavoriteCities(weather => [...weather, data])
+    }
   }
 
 
@@ -57,26 +63,37 @@ function App() {
             <p>{ data.name }</p>
           </div>
           <div className="temp">
-            {/* <h1>{data.main?.temp && `${convertFtoC(data.main?.temp)}°`}</h1> */}
             <h1>{data.main?.temp && `${data.main?.temp}°`}</h1>
           </div>
           <div className="description">
+            {data.name && <button className="add-btn" onClick={()=>handleAddFavoriteCity(data)}><AiOutlinePlusSquare /></button>}
             <p>{ data.weather && data.weather[0].main }</p> 
           </div>
         </div>
+        <div className="middle">
+          {favoriteCities && (<ul>
+            {favoriteCities.map((city) => {
+              return (
+                <li key={city.id}>
+                  <p className="bold">{city.main?.temp && `${city.main?.temp}°`}</p>
+                  <p className="small">{city.name && city.name}</p>
+                  <button className="del-btn"><AiOutlineClose /></button>
+                </li>
+              )
+            })}
+            
+          </ul>)}
+        </div>
         <div className="bottom">
           <div className="feels">
-            {/* <p className="bold">{data.main?.feels_like && `${convertFtoC(data.main?.feels_like)}°`}</p> */}
             <p className="bold">{data.main?.feels_like && `${data.main?.feels_like}°`}</p>
             <p className="small">Feels Like</p>
           </div>
           <div className="temp_min">
-            {/* <p className="bold">{data.main?.temp_min && `${convertFtoC(data.main?.temp_min)}°`}</p> */}
             <p className="bold">{data.main?.temp_min && `${data.main?.temp_min}°`}</p>
             <p className="small">Temparature Min</p>
           </div>
           <div className="temp_max">
-            {/* <p className="bold">{data.main?.temp_max && `${convertFtoC(data.main?.temp_max)}°`}</p> */}
             <p className="bold">{data.main?.temp_max && `${data.main?.temp_max}°`}</p>
             <p className="small">Temparature Max</p>
           </div>
