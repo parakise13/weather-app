f26322bb648bec6e31dcaa05faed9dc7b8bc7aa9
NahyeonKim <parakise13@gmail.com>
2022-06-20 21:23:10 +0900

    final

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ interface WeatherDetailProps {
   main?: string;
 }
 
-interface WeatherMainProps{
+interface WeatherMainProps {
   humidity?: number;
   temp_min?: number;
   temp_max?: number;
@@ -27,78 +27,119 @@ function App() {
   const [favoriteCities, setFavoriteCities] = useState<WeatherProps[]>([]);
   const [location, setLocation] = useState("");
   const APIKey = "64d28b810aabcf14458d21729721bc51";
-	
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIKey}`;
 
-  const searchLocation = (evt:React.KeyboardEvent<HTMLInputElement>) => {
-    if (evt.key === 'Enter') {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIKey}`;
+
+  const searchLocation = (evt: React.KeyboardEvent<HTMLInputElement>) => {
+    if (evt.key === "Enter") {
       axios.get(url).then((res) => {
         setData(res.data);
-        setLocation('');
-      })
+        setLocation("");
+      });
     }
-  }
+  };
 
   const handleChangeValue = (evt: React.ChangeEvent<HTMLInputElement>) => {
     setLocation(evt.target.value);
-  }
+  };
 
   const handleAddFavoriteCity = (data: WeatherProps) => {
-    // let 
-    const existingCityIndex = favoriteCities.findIndex((city) => city.name === data.name);
-    if (existingCityIndex === -1) {
-      setFavoriteCities(weather => [...weather, data])
+    const existingCityIndex = favoriteCities.findIndex(
+      (city) => city.name === data.name
+    );
+    if (favoriteCities.length < 4) {
+      if (existingCityIndex === -1) {
+        setFavoriteCities((weather) => [...weather, data]);
+      }
+    } else if (favoriteCities.length >= 4) {
+      alert("즐겨찾는 도시는 최대 4개까지 추가가 가능합니다.");
     }
-  }
+  };
 
+  const handleRemoveFavoriteCity = (data: WeatherProps) => {
+    const filteredCities = favoriteCities.filter(
+      (city) => city.name !== data.name
+    );
+
+    setFavoriteCities(filteredCities);
+  };
 
   return (
     <div className="app">
       <div className="search">
-        <input type="text" value={location} onChange={handleChangeValue} placeholder='Enter Location' onKeyDown={searchLocation} />
+        <input
+          type="text"
+          value={location}
+          onChange={handleChangeValue}
+          placeholder="Enter Location"
+          onKeyDown={searchLocation}
+        />
       </div>
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>{ data.name }</p>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
             <h1>{data.main?.temp && `${data.main?.temp}°`}</h1>
           </div>
           <div className="description">
-            {data.name && <button className="add-btn" onClick={()=>handleAddFavoriteCity(data)}><AiOutlinePlusSquare /></button>}
-            <p>{ data.weather && data.weather[0].main }</p> 
+            {data.name && (
+              <button
+                className="add-btn"
+                onClick={() => handleAddFavoriteCity(data)}
+              >
+                <AiOutlinePlusSquare />
+              </button>
+            )}
+            <p>{data.weather && data.weather[0].main}</p>
           </div>
         </div>
         <div className="middle">
-          {favoriteCities && (<ul>
-            {favoriteCities.map((city) => {
-              return (
-                <li key={city.id}>
-                  <p className="bold">{city.main?.temp && `${city.main?.temp}°`}</p>
-                  <p className="small">{city.name && city.name}</p>
-                  <button className="del-btn"><AiOutlineClose /></button>
-                </li>
-              )
-            })}
-            
-          </ul>)}
+          {favoriteCities && (
+            <ul>
+              {favoriteCities.map((city) => {
+                return (
+                  <li key={city.id}>
+                    <p className="bold">
+                      {city.main?.temp && `${city.main?.temp}°`}
+                    </p>
+                    <p className="small">{city.name && city.name}</p>
+                    <button
+                      className="del-btn"
+                      onClick={() => handleRemoveFavoriteCity(city)}
+                    >
+                      <AiOutlineClose />
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
         </div>
         <div className="bottom">
           <div className="feels">
-            <p className="bold">{data.main?.feels_like && `${data.main?.feels_like}°`}</p>
+            <p className="bold">
+              {data.main?.feels_like && `${data.main?.feels_like}°`}
+            </p>
             <p className="small">Feels Like</p>
           </div>
           <div className="temp_min">
-            <p className="bold">{data.main?.temp_min && `${data.main?.temp_min}°`}</p>
+            <p className="bold">
+              {data.main?.temp_min && `${data.main?.temp_min}°`}
+            </p>
             <p className="small">Temparature Min</p>
           </div>
           <div className="temp_max">
-            <p className="bold">{data.main?.temp_max && `${data.main?.temp_max}°`}</p>
+            <p className="bold">
+              {data.main?.temp_max && `${data.main?.temp_max}°`}
+            </p>
             <p className="small">Temparature Max</p>
           </div>
           <div className="humidity">
-            <p className="bold">{data.main?.humidity && `${data.main?.humidity}%`}</p>
+            <p className="bold">
+              {data.main?.humidity && `${data.main?.humidity}%`}
+            </p>
             <p className="small">Humidity</p>
           </div>
         </div>
